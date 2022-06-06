7f76a128fd70be7be1da4db04f00ca1522c01fee
Jocelyn Badgley <jocelyn.badgley@zenbusiness.com>
2022-06-06 14:26:03 -0700

    Update runOnce.sh

--- scripts/runOnce.sh
+++ scripts/runOnce.sh
@@ -1,2 +1,4 @@
+#!/bin/sh -e
+
 grep -RiIl 'app-template-ui' . --exclude-dir ./node_modules | xargs sed -i '' -e 's/app-template-ui/'${repo_name}'/g'
 grep -RiIl 'APP_DESCRIPTION_GOES_HERE' . --exclude-dir ./node_modules | xargs sed -i '' -e 's/APP_DESCRIPTION_GOES_HERE/${repo_description}/g'
