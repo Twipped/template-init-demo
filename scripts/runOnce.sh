d09c8be3113d33f840ca9a03702d6a8c1a5aaa89
Jocelyn Badgley <jocelyn.badgley@zenbusiness.com>
2022-06-06 15:51:25 -0700

    final?

--- scripts/runOnce.sh
+++ scripts/runOnce.sh
@@ -1,4 +1,9 @@
 #!/bin/sh -e
 
-grep -RiIl 'app-template-ui' . --exclude-dir ./node_modules | xargs sed -i '' -e 's/app-template-ui/'${repo_name}'/g'
-grep -RiIl 'APP_DESCRIPTION_GOES_HERE' . --exclude-dir ./node_modules | xargs sed -i '' -e 's/APP_DESCRIPTION_GOES_HERE/${repo_description}/g'
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  grep -RiIl 'app-template-ui' . --exclude-dir ./node_modules | xargs sed -i '' -e "s/app-template-ui/${repo_name}/g"
+  grep -RiIl 'APP_DESCRIPTION_GOES_HERE' . --exclude-dir ./node_modules | xargs sed -i '' -e "s/APP_DESCRIPTION_GOES_HERE/${repo_description}/g"
+else
+  grep -RiIl 'app-template-ui' . --exclude-dir ./node_modules | xargs sed -i -e "s/app-template-ui/${repo_name}/g"
+  grep -RiIl 'APP_DESCRIPTION_GOES_HERE' . --exclude-dir ./node_modules | xargs sed -i -e "s/APP_DESCRIPTION_GOES_HERE/${repo_description}/g"
+fi
